f6bcb3e9dbe2711675cdc7546488c5be2ac53b7e
renmak <renmak@gmail.com>
2013-03-09 15:23:05 -0800

    First check in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketo::Application.config.secret_token = 'e8708f922b41f0c8efb0fd98717768e4481eb75495e64d78ca9efee86dac3c5da11cfcf159594ea3f768ca51221dcad79650b093368c134c17f7cb2bead9af04'
